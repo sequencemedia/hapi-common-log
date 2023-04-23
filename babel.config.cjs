de9c23482d1b802523bf3eef33b5e6bcccedc0ef
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-04-23 23:30:26 +0100

    Removes Gulp

--- babel.config.cjs
+++ babel.config.cjs
@@ -14,14 +14,7 @@ const presets = [
   [
     '@babel/env', {
       targets: {
-        node: 'current',
-        browsers: [
-          'last 4 versions',
-          'safari >= 9',
-          'ios >= 8',
-          'ie >= 9',
-          '> 2%'
-        ]
+        node: 'current'
       },
       useBuiltIns: 'usage',
       corejs: 3
