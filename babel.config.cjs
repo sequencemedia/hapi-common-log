58e01342b78697b0cb3acd154d20def501b72729
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-06-01 20:29:10 +0100

    Husky

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,10 +1,18 @@
+const debug = require('debug')
+
+const log = debug('@sequencemedia/hapi-common-log')
+
 const {
   env: {
     NODE_ENV = 'development'
   }
 } = process
 
+log('`@sequencemedia/hapi-common-log` is awake')
+
 function env () {
+  log({ NODE_ENV })
+
   return (
     NODE_ENV === 'production'
   )
